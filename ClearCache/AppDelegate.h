b99d5b8b922dc7ddd6c468e57fad0b6a2811cd51
li125434104 <125434104@qq.com>
2015-07-17 14:47:28 +0800

    Initial Commit

---
+++ ClearCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClearCache
+//
+//  Created by LXJ on 15/7/17.
+//  Copyright (c) 2015年 GOME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClearCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClearCache
+//
+//  Created by LXJ on 15/7/17.
+//  Copyright (c) 2015年 GOME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
